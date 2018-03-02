5333925d1fcd2ecca3b994c7f31fb606aeee9fef
Jake <krazyjakee@gmail.com>
2018-03-02 10:09:54 +0100

    generate imported.js

--- app/client.js
+++ app/client.js
@@ -2,10 +2,11 @@
 // Start your React application and add the required containers
 // Here we have <BrowserRouter /> for react-router
 
-import {rehydrateMarks, assignImportedComponents} from 'react-imported-component';
+import { rehydrateMarks } from 'react-imported-component';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import importedComponents from './imported';
 
 import App from './App';
 
@@ -16,16 +17,16 @@ const app = (
   </BrowserRouter>
 );
 
-// rehydrate the bundle marks
-rehydrateMarks().then(() => {
-  // In production, we want to hydrate instead of render
-  // because of the server-rendering
-  if (process.env.NODE_ENV === 'production') {
+// In production, we want to hydrate instead of render
+// because of the server-rendering
+if (process.env.NODE_ENV === 'production') {
+  // rehydrate the bundle marks
+  rehydrateMarks().then(() => {
     ReactDOM.hydrate(app, element);
-  } else {
-    ReactDOM.render(app, element);
-  }
-});
+  });
+} else {
+  ReactDOM.render(app, element);
+}
 
 // Hot reload is that easy with Parcel
 if (module.hot) {
---
+++ app/imported.js
@@ -0,0 +1,3 @@
+export default {
+      "./app/HelloWorld2": () => import('./HelloWorld2'),
+    }
