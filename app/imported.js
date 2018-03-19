c27935f320e7358aa058dac29fddf0b2d4f9ecc0
Benoit Tremblay <trembl.ben@gmail.com>
2018-03-18 21:58:23 -0400

    Update imported

--- app/imported.js
+++ app/imported.js
@@ -1,7 +1,7 @@
 /* eslint-disable */ 
     import {assignImportedComponents} from 'react-imported-component';
     const applicationImports = {
-      
+      "0": () => import('./HelloWorld2'),
     };
     assignImportedComponents(applicationImports);
     export default applicationImports;
